687a4d039a16a35012bbd8a0842480c22090b490
Djazaanti <alidjazaanti1@gmail.com>
2022-05-01 17:58:53 +0200

    contact : vhost configured : the new domain is http://portfolio.local/

--- index.php
+++ index.php
@@ -11,86 +11,46 @@ use Oc\Blog\service\TwigService;
 
 session_start();
 
-$userController = new UserController(TwigService::getInstance());
-$homeController = new HomeController(TwigService::getInstance());
+// var_dump($_SERVER);
+
+// var_dump($_SERVER['PHP_SELF']);
 
 switch (true) {
     // If nothing in url we load the home page and reset session variable
     case !isset($_SERVER['PATH_INFO']) :
         unset($_SESSION['flash']);
         unset($_SESSION['flash_message']);
+        $homeController = new HomeController(TwigService::getInstance());
         $homeController->showHome();
         break;
     
     // Manage POST form
-    case $_SERVER['REQUEST_METHOD'] = 'POST' :
+    case $_SERVER['REQUEST_METHOD'] == 'POST' :
+        
         // if url is equals to /contact or /#contact
-        if (($_SERVER['PATH_INFO'] = '/contact' || $_SERVER['PATH_INFO'] = '/#contact')) {
-            // echo 'hello de contact formular';
+        if (($_SERVER['PATH_INFO'] == '/contact' || $_SERVER['PATH_INFO'] == '/#contact')) {
+            // echo 'Bonjour'.$_POST['name'];
             $contactController = new ContactController(TwigService::getInstance());
             $contactController->submitFormContact(
                 $_POST['name'],
                 $_POST['lastname'],
                 $_POST['email'],
-                $_POST['message'],
+                $_POST['message']
             );
         }
         break;
-    case $_SERVER['QUERY_STRING'] = 'posts' :
-        echo 'hello posts';
+    case $_SERVER['PATH_INFO'] == '/posts' :
+        $userController = new UserController(TwigService::getInstance());
         $userController->showPosts();
         break;
+        // récupérer un /posts/int 
     case $_GET['action'] == 'post' :
         //  TO DO : vérifier UserModel, si la table est la bonne
+        $userController = new UserController(TwigService::getInstance());
         $userController->showPostAndComments($_GET['id']);
         break;
     // If any case is found
     default:
+    $homeController = new HomeController(TwigService::getInstance());
         $homeController->showHome();
 }
-
-
-
-
-// // var_dump($_SERVER['PATH_INFO']);
-// var_dump($_SERVER['REQUEST_METHOD']);
-// // var_dump($_SERVER['PATH_INFO']);
-// var_dump($_SERVER['QUERY_STRING']);
-// // var_dump($_GET['action']);
-
-// // If nothing in url we load the home page and reset session variable
-// if(!isset($_SERVER['PATH_INFO'])){
-//     unset($_SESSION['flash']);
-//     unset($_SESSION['flash_message']);
-//     $homeController->showHome();
-// }
-    
-// // Manage POST form
-// elseif($_SERVER['REQUEST_METHOD'] = 'POST'){
-//     // if url is equals to /contact or /#contact
-//     // if (($_SERVER['QUERY_STRING'] = '/contact' || $_SERVER['PATH_INFO'] = '/#contact')) {
-//         if ($_SERVER['QUERY_STRING'] = '/contact'){ 
-//             echo 'hello de contact form';
-//         // $contactController = new ContactController(TwigService::getInstance());
-//         // $contactController->submitFormContact(
-//         //     $_POST['name'],
-//         //     $_POST['lastname'],
-//         //     $_POST['email'],
-//         //     $_POST['message'],
-//         // );
-//     }
-// }
-    
-// elseif ($_SERVER['QUERY_STRING'] = '/posts'){
-//     echo 'hello posts';
-//     $userController->showPosts();
-// }
-    
-// elseif ($_GET['action'] == 'post'){
-//     //  TO DO : vérifier UserModel, si la table est la bonne
-//     $userController->showPostAndComments($_GET['id']);
-// }
-    
-// // If any case is found
-// else
-    // $homeController->showHome();
--- src/controller/ContactController.php
+++ src/controller/ContactController.php
@@ -33,19 +33,26 @@ class ContactController
     {
         // check email format. If not valid we send error message
         if (false === filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // if (false === $email ) {
             $_SESSION['flash'] = 'error';
             $_SESSION['flash_message'] = 'Une erreur est survenue. Veuillez vérifier votre email.';
         }
 
         // TODO: send email
+        $recever = 'alidjazaanti1@gmail.com';
+        $subject = 'Formulaire de contact';
+        $headers = 'FROM : '.$email ;
+        mail($recever, $subject, $message, $headers);
 
         // After send email we send success message
-        $_SESSION['flash'] = 'error';
+        $_SESSION['flash'] = 'success';
         $_SESSION['flash_message'] = 'Votre email a bien été envoyé';
-
+        // echo 'formulaire soummis';
+        
         // After sending email we redirect to homepage with contact anchor
-        // header('Location: /#contact');
-        header('Location: ../');
+        header('Location: /#contact');
+        var_dump($_SESSION);
+        exit;
     }
 
 }
